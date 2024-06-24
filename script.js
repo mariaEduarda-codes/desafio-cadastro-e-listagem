09ca8683040fb8bb37e7e6c4ad668bdfe97efdb4
maria <medeiroslima.me@gmail.com>
2024-06-24 10:44:09 -0300

    feat: adds the methods to create and read items on the modal list

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
+const form = document.querySelector('.form');
 const submitButton = document.querySelector('.btn-submit');
 const listModal = document.querySelector('#list-modal');
+const itemList = document.querySelector('#itemList');
 const closeListModalButton = document.querySelector('.close');
 const newProductButton = document.querySelector('#newProductButton');
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-    listModal.style.display = 'block';
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    fetch("http://localhost:8080/product", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(() => {
+            fetch('http://localhost:8080/product')  // Altere a URL conforme necessário
+                .then(response => response.json())
+                .then(data => {
+                    listModal.style.display = 'block';
+                    itemList.innerHTML = '';
+
+                    data.forEach(item => {
+                        const li = document.createElement("li");
+                        const liContent = document.createTextNode(`${item.name} - ${item.price}`);
+                        li.appendChild(liContent);
+                        itemList.appendChild(li);
+                    });
+                })
+                .catch(error => console.error('Erro ao buscar produtos:', error));
+        });
+
 });
 
 closeListModalButton.addEventListener('click', () => {
@@ -21,5 +51,4 @@ window.addEventListener('click', (e) => {
 
 newProductButton.addEventListener('click', () => {
     listModal.style.display = 'none';
-    //reserved for sending to backend logic
 });
